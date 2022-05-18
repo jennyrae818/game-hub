b71d191b0822720621dc02582a4f0aab9e6893fe
aditore <anthonyditore@gmail.com>
2022-05-18 10:42:32 -0700

    adding host for mongo

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/game-hub-db', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/game-hub-db', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
