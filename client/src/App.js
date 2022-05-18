61e47658a430118f9f11cc1033a540fc001586f1
aditore <anthonyditore@gmail.com>
2022-05-18 11:10:41 -0700

    adding basic router structure for react

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+//FOR LATER
+//import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+//import { setContext } from '@apollo/client/link/context';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <>
+      <Routes>
+        <Route path='/' /*element={}*//>
+        <Route/>
+        <Route/>
+      </Routes>
+    </>
+  </Router>
   );
 }
 
