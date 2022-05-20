507bcb344b4aa1d0b7c163760be398ec0ae41d6b
Becca Yu <ugsung8970@gmail.com>
2022-05-19 21:11:08 -0700

    commiting to rebase 1

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom";
 import React from 'react';
 //import './styles/style.css';
+//import LogIn from "../pages/LogIn";
+
+import Auth from "../utils/auth";
+import LogIn from "../pages/LogIn";
 
 
 function NavBar() {
@@ -12,11 +16,20 @@ function NavBar() {
             <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/search">Search</Link>
+            <Link as={Link} to="/search">Search</Link>
           </li>
-            <li>
+          {Auth.loggedIn() ? (
+              <>
+              <Link as={Link} to="/AddGame"></Link>
+              <Link as={Link} to="/Profile"></Link>
+              <Link onClick={Auth.logout}>Logout</Link>
+              </>
+              ) : (
+                <Link onClick={() => LogIn(true)}
+              )} 
+           /*  <li>
             <Link to="/login">Log-In</Link>
-          </li>
+          </li> */
           <li>
             <Link to="/register">Register</Link>
           </li>
