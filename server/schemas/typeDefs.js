6c318b756e9a7820eecf462320b47c0c2251be64
aditore <anthonyditore@gmail.com>
2022-05-19 20:51:40 -0700

    changing structure

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,25 +1,22 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-    type Query {
-        me: User
-    }
-    
-    type User {
-        _id: ID!,
-        username: String!,
-        email: String!,
-        password: String!,
-        games: [Game]
-    }
+type User {
+    _id: ID!,
+    username: String!,
+    email: String!,
+    password: String!,
+    games: [Game]
+}
 
-    type Category {
-        _id: ID,
-        categoryName: String
-    }
 
-    type Game {
-        _id: ID,
+type Category {
+    _id: ID,
+    categoryName: String
+}
+
+type Game {
+    _id: ID,
         gameName: String,
         description: String,
         usersPlaying: Int,
@@ -28,19 +25,19 @@ const typeDefs = gql`
         categories: [Category],
         reviews: [Review]
     }
-
+    
     type Review {
         reviewId: ID,
         reviewBody: String,
         username: String,
         createdAt: String
     }
-
+    
     type Auth {
         token: ID!
         user: User
     }
-
+    
     type Query {
         categories: [Category]
         games: [Game]
@@ -48,7 +45,11 @@ const typeDefs = gql`
         category(id: ID!): Category
         game(id: ID!): Game
     }
-
+    
+    type Query {
+        me: User
+    }
+    
     input GameData {
         gameName: String!
         description: String!
