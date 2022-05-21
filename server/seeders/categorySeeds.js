19b260741b93bde145b007ffeee7811eebf4d923
aditore <anthonyditore@gmail.com>
2022-05-20 22:11:10 -0700

    framework to add seeds to Game

--- server/seeders/categorySeeds.js
+++ server/seeders/categorySeeds.js
@@ -17,4 +17,4 @@
     {
         "categoryName": "Video"
     },
-]
+];
---
+++ server/seeders/gameSeeds.js
@@ -0,0 +1,74 @@
+[
+    {
+        gameName: "",
+        description: "",
+        usersPlaying: "",
+        categories: categories[0]._id
+    },
+    {
+        gameName: "",
+        description: "",
+        usersPlaying: "",
+        categories: categories[0]._id
+    },
+    {
+        gameName: "",
+        description: "",
+        usersPlaying: "",
+        categories: categories[0]._id
+    },
+    {
+        gameName: "",
+        description: "",
+        usersPlaying: "",
+        categories: categories[0]._id
+    },
+    {
+        gameName: "",
+        description: "",
+        usersPlaying: "",
+        categories: categories[0]._id
+    },
+    {
+        gameName: "",
+        description: "",
+        usersPlaying: "",
+        categories: categories[0]._id
+    },
+    {
+        gameName: "",
+        description: "",
+        usersPlaying: "",
+        categories: categories[0]._id
+    },
+    {
+        gameName: "",
+        description: "",
+        usersPlaying: "",
+        categories: categories[0]._id
+    },
+    {
+        gameName: "",
+        description: "",
+        usersPlaying: "",
+        categories: categories[0]._id
+    },
+    {
+        gameName: "",
+        description: "",
+        usersPlaying: "",
+        categories: categories[0]._id
+    },
+    {
+        gameName: "",
+        description: "",
+        usersPlaying: "",
+        categories: categories[0]._id
+    },
+    {
+        gameName: "",
+        description: "",
+        usersPlaying: "",
+        categories: categories[0]._id
+    },
+];
