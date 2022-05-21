95ecb34f18549944d23d9038f225ca427bcef985
marycpriyanka <marycpriyanka@gmail.com>
2022-05-20 19:10:43 -0700

    Add queries in client

---
+++ client/src/utils/queries.js
@@ -0,0 +1,68 @@
+import { gql } from "@apollo/client";
+
+export const QUERY_SINGLE_USER = gql`
+    query singleUser($userId: ID!) {
+        user(userId: $userId) {
+            _id,
+            username,
+            games
+        }
+    }
+`;
+
+export const QUERY_ME = gql`
+    query me {
+        me {
+            _id
+            username,
+            games
+        }
+    }
+`;
+
+export const QUERY_GAMES = gql`
+    query getGames {
+        games {
+            _id,
+            gameName,
+            usersPlaying,
+            thumbsUp,
+            thumbsDown,
+            categories,
+        }
+    }
+`;
+
+export const QUERY_SINGLE_GAME = gql`
+    query getSingleGame($gameId: ID!) {
+        game(gameId: $gameId) {
+            _id,
+            gameName,
+            description,
+            usersPlaying,
+            thumbsUp,
+            thumbsDown,
+            categories,
+            reviews
+        }
+    }
+`;
+
+export const QUERY_CATEGORIES = gql`
+    query getCategories {
+        categories {
+            _id,
+            categoryName
+        }
+    }
+`;
+
+export const QUERY_SINGLE_CATEGORY = gql`
+    query getSingeCategory($categoryId: ID!) {
+        category(categoryId: $categoryId) {
+            _id,
+            categoryName
+        }
+    }
+`;
+
