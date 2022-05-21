7b2c27ad4f8844a6cfd3cdb786187f5f927645a4
aditore <anthonyditore@gmail.com>
2022-05-20 22:20:45 -0700

    fixing category seeds

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,12 +1,20 @@
 const db = require("../config/connection");
-const { Category } = require("../models");
-const categorySeeds = require("./categorySeeds");
+const { Category, Game } = require("../models");
+const categorySeeds = require("./categorySeeds.js");
+const gameSeeds = require("./gameSeeds.js");
 
 db.once("open", async () => {
     try {
         await Category.deleteMany({});
 
         await Category.create(categorySeeds);
+
+        console.log("----CATEGORIES SEEDED----");
+
+        await Game.deleteMany();
+
+        await Game.insertMany(gameSeeds);
+
     } catch (err) {
         console.log(err);
         process.exit(1);
