57fc879a909b81fada3fea5efbad73c346937c68
Becca Yu <ugsung8970@gmail.com>
2022-05-20 22:40:23 -0700

    commiting to rebase develop 2

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,9 +6,10 @@ function Footer() {
   return (
     <footer className="footer">
     
-    <a href= ''><i className="fa">&#xf09b;</i></a>
-    <a href= ''><i className="fa fa-linkedin"></i></a>
-    <a href= ''><i className="fa fa-envelope"></i></a>
+    <a href="https://github.com/jennyrae818"><img src="https://ryanharveyauthor.files.wordpress.com/2018/09/clydeeghost.png" alt="Jenny Github" className="icon"/></a>
+    <a href="https://github.com/aditore"><img src="https://p.kindpng.com/picc/s/98-987350_download-blue-games-ghosts-pac-man-ghost-free.png" alt="Github" className="icon"/></a>
+    <a href="https://github.com/marycpriyanka"><img src="https://img.favpng.com/16/11/25/pac-man-adventures-in-time-pac-mania-pac-man-games-ghosts-png-favpng-DuVjqzpA3GfdN20ZS2wZX2CFw.jpg" alt="Github" className="icon"/></a>
+    <a href="https://github.com/becca6758"><img src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c314.png" alt="Github" className="icon"/></a>
    
   </footer>
   );
