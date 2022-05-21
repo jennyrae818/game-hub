0e47dfe568636023dc5a7c8cbd9bcf674053730a
SACCO <jennyrae818@gmail.com>
2022-05-21 11:36:04 -0700

    front end added updates to remaining pages - basic styling done

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -5,7 +5,27 @@ function Game() {
   return (
     <section className="game">
     
-      <h2>  NAME OF GAME  </h2>
+      <h2>  --NAME OF GAME--  </h2>
+      <h3> Description: </h3>
+      <h3> OverAll Rating: </h3>
+      <table>
+        <tr>
+          <th>Username</th>
+          <th># Times Played</th>
+          <th>Rating</th>
+        </tr>
+        <tr>
+          <td>wonderwoman22</td>
+          <td>40</td>
+          <td> &#9787; </td> 
+       </tr>
+       <tr>
+         <td>wonderman43</td>
+          <td>2</td>
+          <td> &#9785; </td>
+
+        </tr>
+      </table>
            
     </section>
   );
