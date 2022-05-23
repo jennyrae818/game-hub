f7f26d93d62a24af70813d7e79e6960f1647db47
aditore <anthonyditore@gmail.com>
2022-05-22 21:03:14 -0700

    gitttt

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,12 +1,4 @@
 import React, { useState } from 'react';
-<<<<<<< HEAD
-=======
-
-import { useMutation } from '@apollo/client';
-import { ADD_USER } from '../utils/mutations';
-
-import Auth from '../utils/auth';
->>>>>>> 7838f6980b56b26e28477340f8e34d2a9087638c
 
 import { useMutation } from '@apollo/client';
 import { CREATE_USER } from '../utils/mutations';
@@ -28,6 +20,7 @@ function Register(props) {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    console.log(userFormData);
     console.log(createUser({...userFormData}));
     try {
       const newUser = await createUser({
@@ -49,37 +42,6 @@ function Register(props) {
     });
   };
 
-<<<<<<< HEAD
-=======
-function Register() {
-
-  const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
-  
-  //mutation
-  const [addUser] = useMutation(ADD_USER)
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    try {
-      const { data } = await addUser({
-        variables: { ...userFormData },
-      });
-
-      console.log(data);
-      Auth.login(data.addUser.token);
-    } catch (err) {
-      console.error(err);
-    }
-
-    setUserFormData({
-      username: '',
-      email: '',
-      password: '',
-    });
-  };
-
->>>>>>> 7838f6980b56b26e28477340f8e34d2a9087638c
   return (
     <>
     <div className="register">
@@ -88,7 +50,6 @@ function Register() {
       <fieldset>
          <label>
            <p>Username:</p>
-<<<<<<< HEAD
            <input onChange={handleInput} name="username" type="username" placeholder="Unique Username" required />
          </label>
          <label>
@@ -98,17 +59,6 @@ function Register() {
          <label>
            <p>Password:</p>
            <input onChange={handleInput} id="psw" name="password" type="password" minLength="8" placeholder="Enter Password" required/>
-=======
-           <input value={userFormData.username} name="name" placeholder="Unique Username" />
-         </label>
-         <label>
-           <p>Email:</p>
-           <input value={userFormData.email} email="email" placeholder="Enter Email" />
-         </label>
-         <label>
-           <p>Password:</p>
-           <input value={userFormData.password} id="psw" type="password" minlength="8" placeholder="Enter Password" name="psw" required/>
->>>>>>> 7838f6980b56b26e28477340f8e34d2a9087638c
            <p className="pswcrit">*Password must contain letters and numbers and be a minimum of 8 characters</p>
          </label>
        
