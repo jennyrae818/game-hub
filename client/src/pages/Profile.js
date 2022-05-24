284c0aca47f10e05ca361769f9a546e1984af956
aditore <anthonyditore@gmail.com>
2022-05-23 22:47:48 -0700

    starting profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,11 +1,43 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
+import { Navigate, useParams } from 'react-router-dom';
+import Auth from '../utils/auth';
 //import './styles/style.css';
 
+import { QUERY_SINGLE_USER, QUERY_ME } from '../utils/queries';
+
 function Profile() {
+  const { userId } = useParams;
+
+  const { loading, data } = useQuery(
+    userId ? QUERY_SINGLE_USER : QUERY_ME,
+    {
+      variables: { userId: userId }
+    }
+  );
+  
+  const user = data?.me || data?.user || {}; 
+
+  if (Auth.loggedIn() && Auth.getProfile().data._id === userId) {
+    return <Navigate to="/profile" />;
+  }
+
+  if (loading) {
+    return <div>LOADING</div>;
+  }
+
+  if (!user?.username) {
+    return (
+      <h2>
+        Please sign-up or register to view this page!
+      </h2>
+    );
+  }
+  
   return (
     <section className="profile">
     
-      <h2>User Profile</h2>
+      <h2>{`${user.username}`}</h2>
       <h3>My Games</h3>
       <table>
         <tr>
