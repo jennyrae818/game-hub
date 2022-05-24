76cad039f4f7e0af056444a020058cce5131ae86
aditore <anthonyditore@gmail.com>
2022-05-23 22:47:48 -0700

    starting profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
+<<<<<<< HEAD
 //import './styles/style.css';
 
 import { QUERY_ME } from '../utils/queries';
@@ -10,12 +11,45 @@ function Profile() {
   console.log(data);
 
   const user = data?.me || {}; 
+=======
+import { Navigate, useParams } from 'react-router-dom';
+import Auth from '../utils/auth';
+//import './styles/style.css';
+
+import { QUERY_SINGLE_USER, QUERY_ME } from '../utils/queries';
+
+function Profile() {
+  const { userId } = useParams;
+
+  const { loading, data } = useQuery(
+    userId ? QUERY_SINGLE_USER : QUERY_ME,
+    {
+      variables: { userId: userId }
+    }
+  );
+  
+  const user = data?.me || data?.user || {}; 
+
+  if (Auth.loggedIn() && Auth.getProfile().data._id === userId) {
+    return <Navigate to="/profile" />;
+  }
+>>>>>>> 6bea78c (starting profile page)
 
   if (loading) {
     return <div>LOADING</div>;
   }
 
+<<<<<<< HEAD
   console.log(user);
+=======
+  if (!user?.username) {
+    return (
+      <h2>
+        Please sign-up or register to view this page!
+      </h2>
+    );
+  }
+>>>>>>> 6bea78c (starting profile page)
   
   return (
     <section className="profile">
