a4c7e2a8deb6719a746f1e76caed8eb90498a596
aditore <anthonyditore@gmail.com>
2022-05-24 20:20:34 -0700

    profile complete need to be able to add game

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,39 +1,21 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
-import { Navigate, useParams } from 'react-router-dom';
-import Auth from '../utils/auth';
 //import './styles/style.css';
 
-import { QUERY_SINGLE_USER, QUERY_ME } from '../utils/queries';
+import { QUERY_ME } from '../utils/queries';
 
 function Profile() {
-  const { userId } = useParams();
-
-  const { loading, data } = useQuery(
-    userId ? QUERY_SINGLE_USER : QUERY_ME,
-    {
-      variables: { userId: userId }
-    }
-  );
+  const { loading, data } = useQuery(QUERY_ME);
   
-  const user = data?.me || data?.user || {}; 
-  console.log(userId);
-  if (Auth.loggedIn() && Auth.getProfile().data._id === userId) {
-    return <Navigate to="/profile" />;
-  }
+  console.log(data);
+
+  const user = data?.me || {}; 
 
   if (loading) {
     return <div>LOADING</div>;
   }
 
   console.log(user);
-  // if (!user?.username) {
-  //   return (
-  //     <h2>
-  //       Please sign-up or register to view this page!
-  //     </h2>
-  //   );
-  // }
   
   return (
     <section className="profile">
@@ -47,18 +29,14 @@ function Profile() {
           <th># Users Playing</th>
           <th>Rating</th>
         </tr>
-        <tr>
-          <td>Sorry</td>
-          <td>Board</td>
-          <td>26</td>
-          <td>86%</td>
-       </tr>
-       <tr>
-         <td>Cribbage</td>
-          <td>Cards</td>
-          <td>5</td>
-          <td>100%</td>
-        </tr>
+        {user.games && user.games.map(game => (
+          <tr>
+            <td>{game.gameName}</td>
+            <td><ul>{game.categories.map(category => (<li>{category.categoryName}</li>))}</ul></td>
+            <td>{game.usersPlaying}</td>
+            <td>{game.rating}</td>
+          </tr>
+        ))}
       </table>
                 
     </section>
