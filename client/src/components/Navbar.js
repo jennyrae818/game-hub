44674eec2edab634c7bd43c61aece4bc73775f99
marycpriyanka <marycpriyanka@gmail.com>
2022-05-24 18:30:17 -0700

    Added Add-Game and View-Profile menu to navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,36 +7,42 @@ import Auth from '../utils/auth';
 
 function NavBar() {
   return (
-    
+
     <nav className="nav navbar">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/search">Search</Link>
-          </li>
-          {Auth.loggedIn() ? (
-            <>
-              <li>
-                <Link onClick={Auth.logout} to="/">Logout</Link>
-              </li>
-            </>
-          ) : (
-            <>
-              <li>
-                <Link to="/login">Log-In</Link>
-              </li>
-              <li>
-                <Link to="/register">Register</Link>
-              </li>
-            </>
-          )}
-    
-        </ul>
-      </nav>
-     
-    
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/search">Search</Link>
+        </li>
+        {Auth.loggedIn() ? (
+          <>
+            <li>
+              <Link to="/addgame">Add-Game</Link>
+            </li>
+            <li>
+              <Link to="/profile">View-Profile</Link>
+            </li>
+            <li>
+              <Link onClick={Auth.logout} to="/">Logout</Link>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <Link to="/login">Log-In</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </>
+        )}
+
+      </ul>
+    </nav>
+
+
   );
 }
 
