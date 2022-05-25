f429b6748d18c900af9ede06115b37b89aaa8343
Becca Yu <ugsung8970@gmail.com>
2022-05-24 19:50:24 -0700

    commiting for rview

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -16,19 +16,19 @@ function Footer() {
     </div>
 
     <div class="column">
-      <a href="https://github.com/jennyrae818"><img src="../images/pacman_orange.png" alt="Jenny Github" className="icon"/></a>
+      <a href="https://github.com/jennyrae818" target="_blank" rel="noreferrer noopener"><img src="../images/pacman_orange.png" alt="Jenny Github" className="icon"/></a>
     </div>
 
     <div class="column">
-      <a href="https://github.com/aditore"><img src="../images/pacman_blue.png" alt="Anthony Github" className="icon"/></a>
+      <a href="https://github.com/aditore" target="_blank" rel="noreferrer noopener"><img src="../images/pacman_blue.png" alt="Anthony Github" className="icon"/></a>
     </div>
 
     <div class="column">
-      <a href="https://github.com/marycpriyanka"><img src="../images/pacman_pink.png" alt="Priyanka Github" className="icon"/></a>
+      <a href="https://github.com/marycpriyanka" target="_blank" rel="noreferrer noopener"><img src="../images/pacman_pink.png" alt="Priyanka Github" className="icon"/></a>
     </div>
 
     <div class="column">
-      <a href="https://github.com/becca6758"><img src="../images/pacman_red.png" alt="Rebecca Github" className="icon"/></a>
+      <a href="https://github.com/becca6758" target="_blank" rel="noreferrer noopener"><img src="../images/pacman_red.png" alt="Rebecca Github" className="icon"/></a>
     </div>
     
 
