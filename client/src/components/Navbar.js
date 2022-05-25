37abd96013b466f5ad50f65c9530cea4a208202b
Becca Yu <ugsung8970@gmail.com>
2022-05-24 20:30:50 -0700

    corrected navbar for branch review

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,41 +1,48 @@
 import { Link } from "react-router-dom";
 import React from 'react';
 
+import Auth from '../utils/auth';
 //import './styles/style.css';
-//import LogIn from "../pages/LogIn";
-
-import Auth from "../utils/auth";
-import LogIn from "../pages/LogIn";
-//import Register from "../pages/Register";
-//import {'.nav '} from "../App.css";
 
 
-const  NavBar = () => {
-  
+function NavBar() {
   return (
 
     <nav className="nav navbar">
-        <ul className="nav navbar">
-          
-            <li><Link as={Link} to="/">Home</Link></li>
-            <li><Link as={Link} to="/search">Search</Link></li>
-          {}
-          {Auth.loggedIn() ? (
-              <>
-              <li><Link as={Link} to="/AddGame">Add-Game</Link></li>
-              <li><Link as={Link} to="/Profile">Profile</Link></li>
-              <li><Link onClick={Auth.logout}>Logout</Link></li>
-              </>
-              ) : (
-                <li><Link onClick={() => LogIn(true)}>Login</Link></li>
-              ) 
-            } 
-            <li><Link to="/Login">Login</Link></li>
-            <li><Link to="/Register">Register</Link></li>
-        </ul>
-      </nav>
-     
-    
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/search">Search</Link>
+        </li>
+        {Auth.loggedIn() ? (
+          <>
+            <li>
+              <Link to="/addgame">Add-Game</Link>
+            </li>
+            <li>
+              <Link to="/profile">View-Profile</Link>
+            </li>
+            <li>
+              <Link onClick={Auth.logout} to="/">Logout</Link>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <Link to="/login">Log-In</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </>
+        )}
+
+      </ul>
+    </nav>
+
+
   );
 }
 
