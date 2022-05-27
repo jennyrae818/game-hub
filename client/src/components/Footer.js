39da11c9182d1603c49f5119f57dcf93202f5688
Becca Yu <ugsung8970@gmail.com>
2022-05-26 19:44:02 -0700

    commiting after fixing

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -16,19 +16,19 @@ function Footer() {
     </div>
 
     <div class="column">
-      <a href="https://github.com/jennyrae818" target="_blank" rel="noreferrer noopener"><img src="../images/pacman_orange.png" alt="Jenny Github" className="icon"/></a>
+      <a href="https://github.com/jennyrae818"><img src="../images/pacman_orange.png" alt="Jenny Github" className="icon"/></a>
     </div>
 
     <div class="column">
-      <a href="https://github.com/aditore" target="_blank" rel="noreferrer noopener"><img src="../images/pacman_blue.png" alt="Anthony Github" className="icon"/></a>
+      <a href="https://github.com/aditore"><img src="../images/pacman_blue.png" alt="Anthony Github" className="icon"/></a>
     </div>
 
     <div class="column">
-      <a href="https://github.com/marycpriyanka" target="_blank" rel="noreferrer noopener"><img src="../images/pacman_pink.png" alt="Priyanka Github" className="icon"/></a>
+      <a href="https://github.com/marycpriyanka"><img src="../images/pacman_pink.png" alt="Priyanka Github" className="icon"/></a>
     </div>
 
     <div class="column">
-      <a href="https://github.com/becca6758" target="_blank" rel="noreferrer noopener"><img src="../images/pacman_red.png" alt="Rebecca Github" className="icon"/></a>
+      <a href="https://github.com/becca6758"><img src="../images/pacman_red.png" alt="Rebecca Github" className="icon"/></a>
     </div>
     
 
@@ -40,11 +40,19 @@ function Footer() {
     </div>   
 
     
-    <a href="https://github.com/jennyrae818"><img src="https://ryanharveyauthor.files.wordpress.com/2018/09/clydeeghost.png" alt="Jenny Github" className="icon"/></a>
-    <a href="https://github.com/aditore"><img src="https://p.kindpng.com/picc/s/98-987350_download-blue-games-ghosts-pac-man-ghost-free.png" alt="Github" className="icon"/></a>
-    <a href="https://github.com/marycpriyanka"><img src="https://img.favpng.com/16/11/25/pac-man-adventures-in-time-pac-mania-pac-man-games-ghosts-png-favpng-DuVjqzpA3GfdN20ZS2wZX2CFw.jpg" alt="Github" className="icon"/></a>
-    <a href="https://github.com/becca6758"><img src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c314.png" alt="Github" className="icon"/></a>
-   
+
+    </div>
+
+    </div>
+
+{/*   
+    <div class="pacman">
+      <div class="pacman-top"></div>
+      <div class="pacman-bottom"></div>
+      <div class="feed"></div>
+      </div>
+    */}
+
   </footer>
   );
 }
