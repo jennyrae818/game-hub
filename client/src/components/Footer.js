22ac26a91b34944cf41eb6ac5fdeef37df2ae142
Becca Yu <ugsung8970@gmail.com>
2022-05-28 12:18:56 -0700

    commiting changes in footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,38 +5,38 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="footer">
-    <div class="row">
+    <div className="row">
 
-    <div class="column">
-      <div class="pacman">
-      <div class="pacman__eye"></div>
-      <div class="pacman__mouth"></div>
-      <div class="pacman__food"></div> 
+    <div className="column">
+      <div className="pacman">
+      <div className="pacman__eye"></div>
+      <div className="pacman__mouth"></div>
+      <div className="pacman__food"></div> 
       </div>   
     </div>
 
-    <div class="column">
+    <div className="column">
       <a href="https://github.com/jennyrae818" target="_blank" rel="noreferrer noopener"><img src="../images/pacman_orange.png" alt="Jenny Github" className="icon"/></a>
     </div>
 
-    <div class="column">
+    <div className="column">
       <a href="https://github.com/aditore" target="_blank" rel="noreferrer noopener"><img src="../images/pacman_blue.png" alt="Anthony Github" className="icon"/></a>
     </div>
 
-    <div class="column">
+    <div className="column">
       <a href="https://github.com/marycpriyanka" target="_blank" rel="noreferrer noopener"><img src="../images/pacman_pink.png" alt="Priyanka Github" className="icon"/></a>
     </div>
 
-    <div class="column">
+    <div className="column">
       <a href="https://github.com/becca6758" target="_blank" rel="noreferrer noopener"><img src="../images/pacman_red.png" alt="Rebecca Github" className="icon"/></a>
     </div>
     
 
-    <div class="column">
-      <div class="pacman2">
-      <div class="pacman__eye"></div>
-      <div class="pacman__mouth"></div>
-      <div class="pacman__food"></div> 
+    <div className="column">
+      <div className="pacman2">
+      <div className="pacman__eye"></div>
+      <div className="pacman__mouth"></div>
+      <div className="pacman__food"></div> 
     </div>   
 
     
