4c695db8e253f3bd67182bf1d33980d46e7f82cc
aditore <anthonyditore@gmail.com>
2022-05-29 12:19:19 -0700

    final cleanup from tony

--- client/src/pages/LogIn.js
+++ client/src/pages/LogIn.js
@@ -1,9 +1,7 @@
-import React, { useState } from 'react';
-//import './styles/style.css';
-import { useMutation } from '@apollo/client';
-import { LOGIN_USER } from '../utils/mutations';
-
-import Auth from '../utils/auth';
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER } from "../utils/mutations";
+import Auth from "../utils/auth";
 
 function LogIn() {
   //set userFormData state
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,33 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { CREATE_USER } from "../utils/mutations";
+import Auth from "../utils/auth";
 
-import { useMutation } from '@apollo/client';
-import { CREATE_USER } from '../utils/mutations';
-
-import Auth from '../utils/auth';
-
-
-
-function Register(props) {
-
+function Register() {
+  //set userFormData state
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
   
   //mutation
   const [createUser] = useMutation(CREATE_USER);
 
+  //handle userFormData
   const handleInput = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
   };
 
+  //event to handle register
   const handleSubmit = async (event) => {
     event.preventDefault();
-  
+    
+    //await newUser
     try {
       const newUser = await createUser({
         variables: { ...userFormData },
       });
 
-      console.log(newUser);
+      //give new user token
       const token = newUser.data.createUser.token;
       Auth.login(token);
 
@@ -35,6 +34,7 @@ function Register(props) {
       console.error(err);
     }
 
+    //reset form data
     setUserFormData({
       username: '',
       email: '',
@@ -47,27 +47,26 @@ function Register(props) {
     <div className="register">
     <h2>Register Here!</h2>
       <form onSubmit={handleSubmit}>
-      <fieldset>
-         <label>
-           <p>Username:</p>
-           <input onChange={handleInput} name="username" type="username" placeholder="Unique Username" required />
-         </label>
-         <label>
-           <p>Email:</p>
-           <input onChange={handleInput} name="email" type="email" placeholder="Enter Email" />
-         </label>
-         <label>
-           <p>Password:</p>
-           <input onChange={handleInput} id="psw" name="password" type="password" minLength="8" placeholder="Enter Password" required/>
-           <p className="pswcrit">*Password must contain letters and numbers and be a minimum of 8 characters</p>
-         </label>
-       
-       <button type="submit">Submit</button>
-       </fieldset>
+        <fieldset>
+          <label>
+            <p>Username:</p>
+            <input onChange={handleInput} name="username" type="username" placeholder="Unique Username" required />
+          </label>
+          <label>
+            <p>Email:</p>
+            <input onChange={handleInput} name="email" type="email" placeholder="Enter Email" />
+          </label>
+          <label>
+            <p>Password:</p>
+            <input onChange={handleInput} id="psw" name="password" type="password" minLength="8" placeholder="Enter Password" required/>
+            <p className="pswcrit">*Password must contain letters and numbers and be a minimum of 8 characters</p>
+          </label>       
+          <button type="submit">Submit</button>
+        </fieldset>
       </form>
     </div>
     </>
-      );
-    }
+  );
+}
     
-    export default Register;
+export default Register;
