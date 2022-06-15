087cc8bf8e378e58558c6a802cd385dbb68d3af6
aditore <anthonyditore@gmail.com>
2022-06-15 15:05:31 -0700

    adding modal to login, self closes after 1 sec

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,3 +3,4 @@ export { default as Footer } from "./Footer";
 export { default as Navbar } from "./Navbar";
 export { default as ModalS } from "./ModalS";
 export { default as ModalF } from "./ModalF";
+export { default as ModalB } from "./ModalB";
